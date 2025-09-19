3c376882a02dc731a1f884bc8925c7e5b1a0e25a
Abdelkebir Dahmani <abdahman@e1r5p8.1337.ma>
2025-09-19 22:09:23 +0100

    lol

--- api/proxy.js
+++ api/proxy.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`
+                'Authorization': apiKey
             },
             body: JSON.stringify(requestBody )
         });
